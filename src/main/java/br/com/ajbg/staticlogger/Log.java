1e246eb515db3bf9e56d8dbc19e947734ec07949
Alvaro Gomes <ajoathan@gmail.com>
2018-03-07 03:16:26 +0000

    Criacao da interface basica.
    Criada as estruturas do maven e a classe principal do projeto, com os
    principais metodos que ela vai expor. As principais partes desses metodos nao
    foi implementada ainda.
    
    Tambem foi criado o gitignore para ignorar a pasta target do maven.

---
+++ src/main/java/br/com/ajbg/staticlogger/Log.java
@@ -0,0 +1,73 @@
+package br.com.ajbg.staticlogger;
+
+public class Log {
+	private static String pattern = "[$HH:$mm:$ss $yyyy-$MM-$dd] " +
+			"$package.$class#$method: $message";
+
+	public static void format(String pattern) {
+	}
+
+	public static void info(String message) {
+		info(message, null, pattern);
+	}
+
+	public static void info(String message, Object obj) {
+		info(message, obj, pattern);
+	}
+
+	public static void info(String message, String pattern) {
+		info(message, null, pattern);
+	}
+
+	public static synchronized void info(String message,
+			Object obj, String pattern) {
+	}
+
+	public static void warning(String message) {
+		warning(message, null, pattern);
+	}
+
+	public static void warning(String message, Object obj) {
+		warning(message, obj, pattern);
+	}
+
+	public static void warning(String message, String pattern) {
+		warning(message, null, pattern);
+	}
+
+	public static synchronized void warning(String message,
+			Object obj, String pattern) {
+	}
+
+	public static void error(String message) {
+		error(message, null, pattern);
+	}
+
+	public static void error(String message, Object obj) {
+		error(message, obj, pattern);
+	}
+
+	public static void error(String message, String pattern) {
+		error(message, null, pattern);
+	}
+
+	public static synchronized void error(String message,
+			Object obj, String pattern) {
+	}
+
+	public static void error(Exception exp) {
+		error(exp, null, pattern);
+	}
+
+	public static void error(Exception exp, Object obj) {
+		error(exp, obj, pattern);
+	}
+
+	public static void error(Exception exp, String pattern) {
+		error(exp, null, pattern);
+	}
+
+	public static synchronized void error(Exception exp,
+			Object obj, String pattern) {
+	}
+}
