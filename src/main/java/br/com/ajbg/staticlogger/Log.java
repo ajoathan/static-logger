bfca371f5d62ceadaf853a668a4111917be29ff2
Alvaro Gomes <ajoathan@gmail.com>
2018-08-11 02:25:28 +0000

    Criada fila de mensagens.
    Essa fila guarda as mensagens de log até o LogWriter ter condições de
    escrever elas para o arquivo.
    
    Removido o espaço nos logs de info e warning. Setado a versão do java para
    1.8.

--- src/main/java/br/com/ajbg/staticlogger/Log.java
+++ src/main/java/br/com/ajbg/staticlogger/Log.java
@@ -2,9 +2,15 @@ package br.com.ajbg.staticlogger;
 
 import java.io.PrintStream;
 import java.io.FileOutputStream;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
 import java.util.Date;
 
 public class Log {
+	private static BlockingQueue<String> queue =
+			new LinkedBlockingDeque<String>();
+	private static LogWriter writer = new LogWriter(queue);
+
 	private static String logFile = "logfile.log";
 	private static String mainPat =
 			"[%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS][%2$s] ";
@@ -19,13 +25,9 @@ public class Log {
 
 	private static void log(String pattern, String level, Object... msg) {
 		String main = String.format(mainPat, new Date(), level);
-		try {
-			PrintStream ps = new PrintStream(
-					new FileOutputStream(logFile, true));
-			ps.print(main + String.format(pattern, msg) + "\n");
-			ps.close();
-		} catch (Exception e) {
-			e.printStackTrace();
+		queue.add(main + String.format(pattern, msg) + "\n");
+		if (!writer.isAlive()) {
+			writer.start();
 		}
 	}
 
@@ -34,14 +36,35 @@ public class Log {
 	}
 
 	public static void info(String pattern, Object... msg) {
-		log(pattern, " INFO", msg);
+		log(pattern, "INFO", msg);
 	}
 
 	public static void warning(String pattern, Object... msg) {
-		log(pattern, " WARN", msg);
+		log(pattern, "WARN", msg);
 	}
 
 	public static void error(String pattern, Object... msg) {
 		log(pattern, "ERROR", msg);
+	}
+
+	static class LogWriter extends Thread {
+		private BlockingQueue<String> queue;
+
+		public LogWriter(BlockingQueue<String> queue) {
+			this.queue = queue;
+		}
+
+		public void run() {
+			try {
+				PrintStream ps = new PrintStream(
+					new FileOutputStream(logFile, true));
+				while (queue.peek() != null) {
+					ps.print(queue.take());
+				}
+				ps.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
